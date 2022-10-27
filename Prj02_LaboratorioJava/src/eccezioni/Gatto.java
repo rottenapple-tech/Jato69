9c8231d434d1a15bce22f9f022c7bc237f2aa1a5
rottenapple-tech <dam.cyb.act@gmail.com>
2022-10-27 09:56:09 +0200

    ok

--- Prj02_LaboratorioJava/src/eccezioni/Gatto.java
+++ Prj02_LaboratorioJava/src/eccezioni/Gatto.java
@@ -1,7 +1,6 @@
 package eccezioni;
 
 public class Gatto {
-<<<<<<< HEAD
 	String eta;
 	String etaUmana;
 	public Gatto(String eta, String etaUmana) {
@@ -9,17 +8,10 @@ public class Gatto {
 		this.eta = eta;
 		this.etaUmana = etaUmana;
 	}
-=======
 
-	String eta;
-	String etaUmana;
+	
 
-	public Gatto(String eta, String etaUmana) {
-		this.eta = eta;
-		this.etaUmana = etaUmana;
-	}
 
->>>>>>> fc39a57a445dc9984a0880455b8118a1c4221333
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -30,12 +22,6 @@ public class Gatto {
 		builder.append("]");
 		return builder.toString();
 	}
-<<<<<<< HEAD
-=======
 
-	
-	
->>>>>>> fc39a57a445dc9984a0880455b8118a1c4221333
-	
 	
 }
--- Prj02_LaboratorioJava/src/eccezioni/Schiantarsi.java
+++ Prj02_LaboratorioJava/src/eccezioni/Schiantarsi.java
@@ -1,7 +1,7 @@
 package eccezioni;
 
 public class Schiantarsi {
-<<<<<<< HEAD
+
 	public static void main(String[] args) {
 
 		int a = 7;
@@ -22,35 +22,7 @@ public class Schiantarsi {
 		
 		System.out.println("fine");
 	}
-=======
-
-	public static void main(String[] args) {
-		
-
-		int a = 7;
-		int b = 1;
-		
-		try {
-			String s = "clarissa";
-			
-			System.out.println(s.charAt(0)); 
-
-			System.out.println(a / b);
+}		
 			
 			
-			
-		} catch (NullPointerException e) {
-			System.err.println("si è verificata un'eccezione " + e.getMessage());
-		} catch (ArithmeticException e) {
-			System.err.println("si è verificata un'eccezione " + e.getMessage());
-		} catch (Exception e) {
-			System.err.println("si è verificata un'eccezione " + e.getMessage());
-		}
-		
-		
-		System.out.println("sei arrivato alla fine del programma");
-
-	}
-
->>>>>>> fc39a57a445dc9984a0880455b8118a1c4221333
-}
+	
